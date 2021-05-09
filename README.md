3facdb57ac77d202b66df6ce4c488bd0249791c3
Steve Long <steve@Steves-MacBook-Pro.local>
2021-05-08 21:30:25 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rgs-interactive-layout
-interactive utility for React Grid System
+Interactive utility for React Grid System
+
+To view this React + Typescript App, please follow these steps:
+
+1. Clone the repo to your local.ß
+2. From the command prompt, navigate to the folder where the repo is cloned, and invoke the following command: `npm install`
+3. After this, webpack the application by typing `npm webpack`
+4. Finally, serve this application by typing `npm serve` and navigating your browser to the indicated URL.
---
+++ webpack-config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+
+module.exports = [{
+    entry: [path.resolve(__dirname, "./src/index.tsx")],
+    mode: "development",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader?{configFile: \"tsconfig.json\"}",
+                include: [path.resolve(__dirname, "./src"), path.resolve(__dirname, "./components")],
+                options: { allowTsInNodeModules: true }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts",".jsx",".js",]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    // devtool: "source-map",
+    watch: false
+}];
