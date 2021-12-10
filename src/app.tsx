029f37097f55d39c1e80c28acd43e961a551a479
steven omicron <stevelong@prpl.rs>
2021-12-10 09:29:33 -0600

    remove inline style in favor of class names

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import FlexLayoutUtility from "../components/FlexLayoutUtility";
 
 export const App = (): JSX.Element => <>
-            <div className="container-fluid" style={{ backgroundColor: "#7c3392", color: "white", padding: "1rem", marginBottom: "1rem" }}>
-                <h3><pre style={{ color: 'white', marginBottom: 0 }}>Flex layout utility</pre></h3>
+            <div className="container-fluid title-bar">
+                <h3><pre className="preformatted-pagetitle">Flex layout utility</pre></h3>
             </div>
             <div className="container">
                 <br />
