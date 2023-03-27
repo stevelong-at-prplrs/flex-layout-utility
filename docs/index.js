bb6830e60e5f0426f432616bd1e9d6f61f589509
Steve Long <stevelong@prpl.rs>
2023-03-26 20:12:58 -0500

    remove files emitted

--- docs/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOMClient = require("react-dom/client");
-const app_1 = require("./app");
-ReactDOMClient.createRoot(document.getElementById('app') || document.body).render(React.createElement(app_1.App, null));
