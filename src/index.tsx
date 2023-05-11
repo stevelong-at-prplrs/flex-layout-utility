8acdc8691de7c19a1feae9557ad707fe5a06ad75
Steve Long <stevelong@prpl.rs>
2023-05-10 21:37:10 -0500

    add stlye

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { App } from "./app";
 
-const queryParameters = new URLSearchParams(window.location.search)
-const theme = queryParameters.get('t');
-const root = ReactDOMClient.createRoot(document.getElementById('app'));
+const root = ReactDOMClient.createRoot(document.getElementById('app') || document.body);
 root.render(<App theme={'loading'}/>);
+
+const queryParameters = new URLSearchParams(window.location.search)
+const theme = queryParameters.get('t') ?? '';
 
 setTimeout(() => {
     root.render(<App theme={theme} />);
